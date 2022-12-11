cad17cfc91bc54bf50dc4cb8b7034414fa664742
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-11 18:31:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int d=2 , i=0 , n ;
+    cin>> d ;
+    float num [d] ;
+    for (i=0 ;i<d ; i++) {
+        cin >> n ;
+    }
+    float x = array( d , num ) ;
+    cout << x << endl;
   return 0;
 }
