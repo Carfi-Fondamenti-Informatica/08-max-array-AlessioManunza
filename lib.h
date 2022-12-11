4499b29de4bf2c7233e802a20858b9545d84ade9
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-11 22:19:10 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,7 +1,5 @@
 //
 // Created by Alessio on 11/12/2022.
 //
-#ifndef MAX_ARRAY_LIB_H
-#define MAX_ARRAY_LIB_H
-float array (float [] , int );
-#endif //MAX_ARRAY_LIB_H
+
+float array (float [] , int d);
