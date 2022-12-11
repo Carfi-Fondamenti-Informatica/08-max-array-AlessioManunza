a4a0269a5e206577e4b8fefadb1e6391cd821480
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-11 18:24:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+//
+// Created by Alessio on 11/12/2022.
+//
+#include "lib.h"
 
+float array (int d , float num[d]) {
+    int i=1;
+    while (i < d) {
+        if (num [0] > num [i]) {
+            i++ ;
+        }
+        else {
+            num[0]=num[i] ;
+            i++ ;
+        }
+    }
+    return num [0] ;
+}
